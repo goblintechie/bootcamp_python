9af28759c099663833de9ea2cd215191f77bf486
baoyg <25859740+baoyg@users.noreply.github.com>
2025-05-10 23:02:43 +0800

    Day 1

---
+++ Day 1 - Beginner - Working with Variables In Python to Manage Data/Band Name Generator.py
@@ -0,0 +1,9 @@
+# Create a greeting for you program.
+# Ask the user for the city that they grew up in and store it in a variable.
+# Ask the user for the name of a pet and store it in a variable.
+# Combine the name of their city and pet and show them their band name.
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
---
+++ Day 1 - Beginner - Working with Variables In Python to Manage Data/task.py
@@ -0,0 +1,10 @@
+# write your code below line
+# print("Hello world!")
+# print("Hello" + " " + "Angela")
+
+# print("Hello " + input("What is your name?"))
+
+name = input("What is your name?")
+length = len(name)
+print(name)
+print(length)
