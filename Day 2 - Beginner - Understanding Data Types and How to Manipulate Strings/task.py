af2047824fe8ec3fc3eda08a2a517f4766a0346d
baoyg <25859740+baoyg@users.noreply.github.com>
2025-05-11 18:11:48 +0800

    update Day 2

---
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/task.py
@@ -0,0 +1,34 @@
+"""
+Data Types
+"""
+
+# print("hello"[0])
+# print("123" + "456")
+# print(123+456)
+
+# print(len("hello"))
+
+# print(type(123))
+# print(type(3.14))
+# print(type(True))
+
+# print(int("123") + int("345"))
+
+# length = len(input("Enter your name:"))
+# print("Number of letters in your name: " + str(length))
+
+# print("My age: " + str(30))
+# print(3 * 3 + 3 / 3 - 3)
+
+# weight = 84
+# height = 1.65
+# bmi = weight / height ** 2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi))
+# print(round(bmi,2))
+
+score = 0
+height = 1.8
+is_winning = True
+print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}.")
